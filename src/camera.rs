9c821b3279acecb15020736b469b5cdd31fdcef0
abcdefgh <abcdefgh@domain.com>
2021-09-03 10:52:59 -0400

    fix: cargo clippy

--- src/camera.rs
+++ src/camera.rs
@@ -75,7 +75,7 @@ pub fn pan_orbit_camera(
             let yaw = Quat::from_rotation_y(-delta_x);
             let pitch = Quat::from_rotation_x(-delta_y);
             transform.rotation = yaw * transform.rotation; // rotate around global y axis
-            transform.rotation = transform.rotation * pitch; // rotate around local x axis
+            transform.rotation *= pitch; // rotate around local x axis
         } else if pan.length_squared() > 0.0 {
             any = true;
             // make panning distance independent of resolution and FOV,
@@ -107,8 +107,7 @@ pub fn pan_orbit_camera(
 
 fn get_primary_window_size(windows: &Res<Windows>) -> Vec2 {
     let window = windows.get_primary().unwrap();
-    let window = Vec2::new(window.width() as f32, window.height() as f32);
-    window
+    Vec2::new(window.width() as f32, window.height() as f32)
 }
 
 pub fn spawn_camera(commands: &mut Commands) {

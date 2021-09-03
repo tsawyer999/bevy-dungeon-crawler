cb024a80c9b5c5d45c3ca96559ab3dbecd2d5673
Sebastien DErrico <sebastien@hollox.net>
2021-09-03 15:12:26 -0400

    feat: move camera (#2)
    * feat: import pan+orbit example
    
    * refactor: split setup function
    
    * refactor: move light into a seperate file
    
    * feat: spawn a camera from camera mod
    
    * feat: spawn a camera from camera mod
    
    * feat: able to control camera with moue
    
    * refactor: cargo fmt
    
    * fix: cargo clippy
    
    * fix: remove commented code
    
    * fix: convert f32 to f64
    
    * fix: reset f64 to f32
    
    * fix: refactor rotator_system
    
    * refactor: remove create keyword in imports
    
    * fix: file endligne
    
    Co-authored-by: abcdefgh <abcdefgh@domain.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod camera;
+mod light;
+
+use camera::spawn_camera;
+use light::Rotates;
 use bevy::{pbr::AmbientLight, prelude::*};
 
 fn main() {
@@ -9,31 +14,33 @@ fn main() {
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
-        .add_system(rotator_system.system())
+        .add_system(light::rotator_system.system())
+        .add_system(camera::pan_orbit_camera.system())
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let scene = asset_server.load("models/bat.gltf#Scene0");
+    spawn_creature(asset_server, &mut commands, "models/bat.gltf#Scene0");
+
+    spawn_camera(&mut commands);
+
+    spawn_light(&mut commands);
+}
+
+fn spawn_creature(
+    asset_server: Res<AssetServer>,
+    commands: &mut Commands,
+    model_path: &'static str,
+) {
+    let scene = asset_server.load(model_path);
     commands.spawn_scene(scene);
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_xyz(-1.0,1.0,-1.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
-        ..Default::default()
-    });
+}
+
+fn spawn_light(commands: &mut Commands) {
     commands
         .spawn_bundle(LightBundle {
             transform: Transform::from_xyz(3.0, 5.0, 3.0),
             ..Default::default()
         })
         .insert(Rotates);
-}
-
-struct Rotates;
-
-fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
-    for mut transform in query.iter_mut() {
-        *transform = Transform::from_rotation(Quat::from_rotation_y(
-            (4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds(),
-        )) * *transform;
-    }
 }

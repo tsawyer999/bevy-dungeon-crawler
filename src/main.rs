6c031cf00ecbe17f1ffe2caadf5214ead0a60268
abcdefgh <abcdefgh@domain.com>
2021-09-03 13:04:12 -0400

    feat: able to render 3d model in web

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,22 @@ use bevy::{pbr::AmbientLight, prelude::*};
 fn main() {
     let mut app = App::build();
 
-    app.add_plugins(DefaultPlugins);
+    // app.add_plugins(DefaultPlugins);
+    app.insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins);
 
     // when building for Web, use WebGL2 rendering
     #[cfg(target_arch = "wasm32")]
         app.add_plugin(bevy_webgl2::WebGL2Plugin);
 
-    // TODO: add all your other stuff to `app` as usual
-
-    app.run();
-
-    // let mut app = App::build();
-    // app.insert_resource(Msaa { samples: 4 })
-    //     .add_plugins(DefaultPlugins);
-    // #[cfg(target_arch = "wasm32")]
-    // app.add_plugin(bevy_webgl2::WebGL2Plugin);
-    //
-    // app.insert_resource(AmbientLight {
-    //         color: Color::WHITE,
-    //         brightness: 1.0 / 5.0f32,
-    //     })
-    //     .insert_resource(Msaa { samples: 4 })
-    //     .add_plugins(DefaultPlugins)
-    //     .add_startup_system(setup.system())
-    //     .add_system(light::rotator_system.system())
-    //     .add_system(camera::pan_orbit_camera.system())
-    //     .run();
+    app.insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 1.0 / 5.0f32,
+        })
+        .add_startup_system(setup.system())
+        .add_system(light::rotator_system.system())
+        .add_system(camera::pan_orbit_camera.system())
+        .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {

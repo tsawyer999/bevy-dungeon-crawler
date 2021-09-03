f2d47b90ae02a40f1e0f9cc3b05fc6af19b417b6
abcdefgh <abcdefgh@domain.com>
2021-09-03 14:43:38 -0400

    fix: refactor rotator_system

--- src/light.rs
+++ src/light.rs
@@ -5,8 +5,6 @@ pub struct Rotates;
 pub fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
     for mut transform in query.iter_mut() {
         let y_position = (4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds();
-        *transform = Transform::from_rotation(Quat::from_rotation_y(
-            y_position,
-        )) * *transform;
+        *transform = Transform::from_rotation(Quat::from_rotation_y(y_position)) * *transform;
     }
 }

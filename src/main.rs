0557978add66a91525fa65f3af8369e0e3a49575
abcdefgh <abcdefgh@domain.com>
2021-09-18 12:12:39 -0400

    refactor: move light function to a module

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod camera;
-mod rotator_light;
+mod light;
+mod rotator;
 mod mesh;
 
 use bevy::{pbr::AmbientLight, prelude::*};
 use camera::spawn_camera;
-use rotator_light::Rotates;
 use bevy_mod_picking::{PickingPlugin, InteractablePickingPlugin, HighlightablePickingPlugin};
 
 fn main() {
@@ -19,7 +19,7 @@ fn main() {
         .add_plugin(InteractablePickingPlugin)
         .add_plugin(HighlightablePickingPlugin)
         .add_startup_system(setup.system())
-        .add_system(rotator_light::light.system())
+        .add_system(rotator::rotate.system())
         .add_system(camera::pan_camera.system())
         .add_system(camera::orbit_camera.system())
         .add_system(camera::scroll_camera.system())
@@ -34,7 +34,6 @@ fn setup(
 ) {
     let stone_material = materials.add(Color::rgb(0.5, 0.5, 0.5).into());
     let creature_mesh = asset_server.load("models/bat.gltf#Mesh0/Primitive0");
-    let plane_mesh = meshes.add(Mesh::from(shape::Plane { size: 5.0 }));
 
     mesh::spawn_mesh(
         &mut commands,
@@ -56,16 +55,8 @@ fn setup(
         Vec3::new(0.0, 0.0, 0.0),
     );
 
-    spawn_light(&mut commands, Vec3::new(3.0, 5.0, 3.0));
+    light::spawn_rotator_light(&mut commands, Vec3::new(3.0, 5.0, 3.0));
 
+    let plane_mesh = meshes.add(Mesh::from(shape::Plane { size: 5.0 }));
     mesh::spawn_plane(&mut commands, plane_mesh.clone(), stone_material.clone());
-}
-
-fn spawn_light(commands: &mut Commands, position: Vec3) {
-    commands
-        .spawn_bundle(LightBundle {
-            transform: Transform::from_translation(position),
-            ..Default::default()
-        })
-        .insert(Rotates);
 }
--- src/rotator.rs
+++ src/rotator.rs
@@ -2,7 +2,7 @@ use bevy::prelude::*;
 
 pub struct Rotates;
 
-pub fn light(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
+pub fn rotate(time: Res<Time>, mut query: Query<&mut Transform, With<Rotates>>) {
     for mut transform in query.iter_mut() {
         let y_position = (4.0 * std::f32::consts::PI / 20.0) * time.delta_seconds();
         *transform = Transform::from_rotation(Quat::from_rotation_y(y_position)) * *transform;
